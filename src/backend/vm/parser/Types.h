5ad718ef9491d2a506f136c92c5914f1fbeaec96
cetusw <kugelev.m@gmail.com>
2026-06-10 21:39:30 +0300

    c-grade добавил новые опкоды для ссылок

--- src/backend/vm/parser/Types.h
+++ src/backend/vm/parser/Types.h
@@ -36,9 +36,11 @@ static const std::unordered_map<std::string, OpCodeInfo> INSTRUCTIONS = {
 	{ "const", { OP_CONSTANT, OperandType::BYTE } },
 	{ "get_local", { OP_GET_LOCAL, OperandType::BYTE } },
 	{ "set_local", { OP_SET_LOCAL, OperandType::BYTE } },
+	{ "ref_local", { OP_REF_LOCAL, OperandType::BYTE } },
 	{ "def_global", { OP_DEFINE_GLOBAL, OperandType::BYTE } },
 	{ "get_global", { OP_GET_GLOBAL, OperandType::BYTE } },
 	{ "set_global", { OP_SET_GLOBAL, OperandType::BYTE } },
+	{ "ref_global", { OP_REF_GLOBAL, OperandType::BYTE } },
 	{ "call", { OP_CALL, OperandType::BYTE } },
 
 	{ "jump", { OP_JUMP, OperandType::SHORT } },
